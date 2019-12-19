0b3176a38b8065d6b7fe2e7430ac849c807c49ed
Srikanth <Gali_Srikanth@msrlimited.com>
2019-12-19 21:44:27 +0530

    2nd level files

---
+++ ProjectStructure/MVC/Controller/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ProjectStructure
+//
+//  Created by Gali Srikanth on 19/12/19.
+//  Copyright © 2019 Sagarika. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
